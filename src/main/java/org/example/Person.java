3f0cd006dd98bad9b8526957a7a3a524836674d0
Jzakka <mouse4786@gmail.com>
2023-04-18 16:30:54 +0900

    bugFix: 기본자료형, 래퍼클래스 파라미터가 섞여도 작동하게 수정

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -4,8 +4,8 @@ public class Person{
     private int age = 10;
     private String name = "폴";
     private int getAge() { return age; }
-    private int getAge(Integer no1) { return age + no1; }
-    private int getAge(int no1, int no2) { return age + no1 + no2; }
+    private int getAge(int no1) { return age + no1; }
+    private int getAge(Integer no1, int no2) { return age + no1 + no2; }
     private int getAge(String no1) { return age + Integer.parseInt(no1); }
     private String getName() { return name; }
     private String getName(String subfix) { return name + subfix; }
--- src/main/java/org/example/TestUtil.java
+++ src/main/java/org/example/TestUtil.java
@@ -1,12 +1,11 @@
 package org.example;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestUtil {
     private static final Map<Class<?>, Class<?>> wrapperToPrimitiveMap = new HashMap<>();
+    private static final Map<Class<?>, Class<?>> primitiveToWrapperMap = new HashMap<>();
 
     static {
         wrapperToPrimitiveMap.put(Integer.class, int.class);
@@ -17,26 +16,76 @@ public class TestUtil {
         wrapperToPrimitiveMap.put(Character.class, char.class);
         wrapperToPrimitiveMap.put(Byte.class, byte.class);
         wrapperToPrimitiveMap.put(Short.class, short.class);
+
+        primitiveToWrapperMap.put(int.class, Integer.class);
+        primitiveToWrapperMap.put(long.class, Long.class);
+        primitiveToWrapperMap.put(double.class, Double.class);
+        primitiveToWrapperMap.put(float.class, Float.class);
+        primitiveToWrapperMap.put(boolean.class, Boolean.class);
+        primitiveToWrapperMap.put(char.class, Character.class);
+        primitiveToWrapperMap.put(byte.class, Byte.class);
+        primitiveToWrapperMap.put(short.class, Short.class);
     }
 
     public <T> T run(Object target, String methodName, Object... params) throws Exception {
-        Method method;
-        try {
-            Class<?>[] paramTypes = Arrays.stream(params)
-                    .map(Object::getClass)
-                    .toArray(Class[]::new);
-            method = target.getClass().getDeclaredMethod(methodName, paramTypes);
+        Class<?>[] paramTypes = Arrays.stream(params)
+                .map(Object::getClass)
+                .toArray(Class[]::new);
 
-        } catch (NoSuchMethodException e) {
-            Class<?>[] boxedParamTypes = Arrays.stream(params)
-                    .map(p -> {
-                        Class<?> pClass = p.getClass();
-                        return wrapperToPrimitiveMap.getOrDefault(pClass, pClass);
-                    })
-                    .toArray(Class[]::new);
-            method = target.getClass().getDeclaredMethod(methodName, boxedParamTypes);
+        List<Class<?>[]> possibleParamTypes = generatePossibleParamTypes(paramTypes);
+
+        for (Class<?>[] possibleParamType : possibleParamTypes) {
+            Optional<Method> methodOptional = findMethod(target.getClass(), methodName, possibleParamType);
+            if (methodOptional.isPresent()) {
+                Method method = methodOptional.get();
+                method.setAccessible(true);
+                return (T) method.invoke(target, params);
+            }
         }
-        method.setAccessible(true);
-        return (T) method.invoke(target, params);
+
+        throw new NoSuchMethodException(
+                "No such method found: " + methodName + " with parameter types " + Arrays.toString(paramTypes));
     }
+
+    private Optional<Method> findMethod(Class<?> clazz, String methodName, Class<?>[] paramTypes) {
+        try {
+            return Optional.of(clazz.getDeclaredMethod(methodName, paramTypes));
+        } catch (NoSuchMethodException e) {
+            return Optional.empty();
+        }
+    }
+
+    private List<Class<?>[]> generatePossibleParamTypes(Class<?>[] paramTypes) {
+        List<Class<?>[]> possibleParamTypesList = new ArrayList<>();
+        generatePossibleParamTypes(paramTypes, 0, possibleParamTypesList);
+        return possibleParamTypesList;
+    }
+
+    private void generatePossibleParamTypes(Class<?>[] paramTypes, int index, List<Class<?>[]> possibleParamTypesList) {
+        if (index == paramTypes.length) {
+            possibleParamTypesList.add(paramTypes.clone());
+            return;
+        }
+        Class<?> originalType = paramTypes[index];
+        Class<?> primitiveType = wrapperToPrimitiveMap.get(originalType);
+        Class<?> wrapperType = primitiveToWrapperMap.get(originalType);
+
+        // 일반 오브젝트임
+        if (primitiveType == null && wrapperType == null) {
+            generatePossibleParamTypes(paramTypes, index + 1, possibleParamTypesList);
+        }
+        // 래퍼 혹은 기본 클래스임
+        else{
+            if (primitiveType != null) { // 래퍼클래스임
+                paramTypes[index] = primitiveType;
+            } else  { // 기본클래스임
+                paramTypes[index] = wrapperType;
+            }
+            generatePossibleParamTypes(paramTypes, index + 1, possibleParamTypesList);
+            paramTypes[index] = originalType;
+            generatePossibleParamTypes(paramTypes, index + 1, possibleParamTypesList);
+        }
+    }
+
+
 }
