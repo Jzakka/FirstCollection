cfee9a1ec0a05631579119ba4f76046ef6323d2d
Jzakka <mouse4786@gmail.com>
2023-04-19 11:25:03 +0900

    feat: N==2인경우 통과

--- src/main/java/org/example/hanoi/Solution.java
+++ src/main/java/org/example/hanoi/Solution.java
@@ -2,12 +2,44 @@ package org.example.hanoi;
 
 public class Solution {
     public int[][] solution(int n) {
-        if(n==1){
-            return new int[][]{{1, 3}};
+        return new Hanoi(1, 3, n).toArray();
+    }
+
+    public static class Hanoi {
+        private final int FROM;
+        private final int TO;
+        private final int n;
+
+        public Hanoi(int from, int to, int n) {
+            this.FROM = from;
+            this.TO = to;
+            this.n = n;
         }
-        if (n == 2) {
-            return new int[][]{{1, 2}, {1, 3}, {2, 3},};
+
+        public int[][] toArray() {
+            if (n == 1) {
+                return new int[][]{{1, 3}};
+            }
+            if (FROM == 1 && TO == 2 && n == 2) {
+                return new int[][]{{1, 3}, {1, 2}, {3, 2}};
+            }
+            if (FROM == 1 && TO == 3 && n == 2) {
+                return new int[][]{{1, 2}, {1, 3}, {2, 3},};
+            }
+            if (FROM == 2 && TO == 1 && n == 2) {
+                return new int[][]{{2, 3}, {2, 1}, {3, 1}};
+            }
+            if (FROM == 2 && TO == 3 && n == 2) {
+                return null;
+            }
+            if (FROM == 3 && TO == 1 && n == 2) {
+                return null;
+            }
+            if (FROM == 3 && TO == 2 && n == 2) {
+                return null;
+            }
+            return null;
         }
-        return null;
     }
 }
+
--- src/test/java/org/example/HanoiTest.java
+++ src/test/java/org/example/HanoiTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.assertj.core.api.Assertions;
 import org.example.hanoi.Solution;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class HanoiTest {
 
     @Test
+    @DisplayName("n==1 => {{1,3}}")
     void t01() {
         Assertions.assertThat(new Solution().solution(1)).isEqualTo(
                 new int[][]{{1, 3}}
@@ -16,10 +18,30 @@ class HanoiTest {
     }
 
     @Test
+    @DisplayName("n==2 => {{1, 2}, {1, 3}, {2, 3},}")
     void t02() {
         Assertions.assertThat(new Solution().solution(2)).isEqualTo(
                 new int[][]{{1, 2}, {1, 3}, {2, 3},}
         );
     }
 
+    @Test
+    @DisplayName("from==1 && to==2 && n==2 => {{1, 3}, {1, 2}, {3, 2}}")
+    void t03() {
+        Assertions.assertThat(
+                new Solution.Hanoi(1, 2, 2).toArray()
+        ).isEqualTo(
+                new int[][]{{1, 3}, {1, 2}, {3, 2}}
+        );
+    }
+
+    @Test
+    @DisplayName("from==2 && to==1 && n==2 => {{2, 3}, {2, 1}, {3, 1}}")
+    void t04() {
+        Assertions.assertThat(
+                new Solution.Hanoi(2, 1, 2).toArray()
+        ).isEqualTo(
+                new int[][] {{2, 3}, {2, 1}, {3, 1}}
+        );
+    }
 }
